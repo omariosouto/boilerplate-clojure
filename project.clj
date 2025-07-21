dbe5c5a782d7138b19808d3ec05974d0da3c1bb4
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2025-07-21 13:32:31 -0300

    update

--- project.clj
+++ project.clj
@@ -4,7 +4,11 @@
   :license {:name "MIT"}
   :dependencies [[org.clojure/clojure "1.12.1"]
                  [cheshire "5.10.0"]
-                 [nubank/state-flow "5.20.1"]]
+                 [nubank/state-flow "5.20.1"]
+                 [io.pedestal/pedestal.jetty "0.7.0"]
+                 [org.clojure/data.json "2.4.0"]
+                 [org.slf4j/slf4j-simple "2.0.17"]
+                 ]
   :source-paths ["src"]
   :main boilerplate-clojure.server
   
--- src/boilerplate_clojure/server.clj
+++ src/boilerplate_clojure/server.clj
@@ -1,7 +1,58 @@
-(ns boilerplate-clojure.server)
+(ns boilerplate-clojure.server
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]
+            [io.pedestal.test :as test]))
 
 (defn sum [a b]
   (+ a b))
 
+(defn response [status body & {:as headers}]
+  {:status status :body body :headers headers})
+
+(def echo
+  {:name :echo
+   :enter
+   (fn [context]
+     (let [request (:request context)
+           response (response 200 "Hello from Pedestal!")]
+       (assoc context :response response)))})
+
+(def routes
+  (route/expand-routes
+   #{["/" :get echo :route-name :list-query-form-root]}))
+
+; ================================
+; CONFIG
+; ================================
+
+(def service-map
+  {::http/routes routes
+   ::http/type :jetty
+   ::http/port 8080})
+
+(defn start []
+  (http/start (http/create-server service-map)))
+
+;; For interactive development
+(defonce server (atom nil))
+
+(defn start-dev []
+  (reset! server
+          (http/start (http/create-server
+                       (assoc service-map
+                              ::http/join? false)))))
+
+(defn stop-dev []
+  (http/stop @server))
+
+(defn restart []
+  (stop-dev)
+  (start-dev))
+
+(defn test-request [verb url]
+  (io.pedestal.test/response-for (::http/service-fn @server) verb url))
+
 (defn -main [& _args]
-  (println (sum 1 2)))
+  (println (sum 1 2))
+  ;; https://pedestal.io/pedestal/0.6/guides/your-first-api.html
+  (start))
