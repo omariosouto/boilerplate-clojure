ad2a55fda061a4f72cfd549aed9bd7fbb1a0b4f9
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2025-07-21 12:36:46 -0300

    update

--- test/boilerplate_clojure/integration/server_test.clj
+++ test/boilerplate_clojure/integration/server_test.clj
@@ -1,14 +1,26 @@
 (ns boilerplate-clojure.integration.server-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.test :refer [is]]
+            [state-flow.api :refer [defflow flow]]
             [io.pedestal.test :refer [response-for]]
             [io.pedestal.http :as http]
             [boilerplate-clojure.server :as server]))
 
-(deftest home-page-responds
+(defn make-request [{:keys [path method body]}]
   (let [servlet (http/create-servlet (server/service-map))
-        response (response-for servlet :get "/")]
+        response (response-for servlet method path body)]
+    response))
+
+(println (make-request {:path "/"
+                        :method :get
+                        :body "Hello from Pedestal!"}))
+
+(defflow home-page-responds
+  (let [response (make-request {:path "/"
+                                :method :get
+                                :body "Hello from Pedestal!"})]
     (is (= 200 (:status response)))
     (is (= "Hello from Pedestal!" (:body response)))))
 
-(deftest simple-math-works
-  (is (= 4 (+ 2 2)))) 
+(defflow simple-math-works
+  (flow "simple math test"
+        (is (= 4 (+ 2 2)))))
